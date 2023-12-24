d8dde9c8390d08a2371e805658236c974c3f1f74
Trần Quốc Thịnh <43133530+quocthinhred@users.noreply.github.com>
2023-12-24 14:31:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,17 +10,114 @@ const wrongAnswers = document.querySelectorAll(".wrong-answer");
 const congratsAudio = document.querySelector(".congrats-audio");
 const wrongAudio = document.querySelector(".wrong-audio");
 const thanks = document.querySelector(".thanks");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+
+const step = [
+  "START",
+  "VIDEO",
+  "QUESTION1",
+  "QUESTION2",
+  "END"
+]
+
+let currentStep = "START";
+
+const displayBlock = (element) => {
+  element.style.display = "block";
+}
+
+const displayFlex = (element) => {
+  element.style.display = "flex";
+}
+
+const displayNone = (element) => {
+  element.style.display = "none";
+}
+
+const changeStep = (step) => {
+  switch(step) {
+    case "START":
+      displayBlock(playButton);
+      displayNone(video);
+      displayNone(question1);
+      displayNone(question2);
+      displayNone(thanks);
+      currentStep = "START";
+      displayNone(prevBtn);
+      displayNone(questionWrapper);
+      displayNone(nextBtn);
+      break;
+    case "VIDEO":
+      displayNone(playButton);
+      displayBlock(video);
+      video.play();
+      displayNone(question1);
+      displayNone(question2);
+      displayNone(thanks);
+      displayBlock(prevBtn);
+      displayBlock(nextBtn);
+      displayNone(questionWrapper);
+      currentStep = "VIDEO";
+      break;
+    case "QUESTION1":
+      displayNone(playButton);
+      displayNone(video);
+      displayFlex(question1);
+      displayNone(question2);
+      displayNone(thanks);
+      displayBlock(prevBtn);
+      displayBlock(nextBtn);
+      displayBlock(questionWrapper);
+      currentStep = "QUESTION1";
+      break;
+    case "QUESTION2":
+      displayNone(playButton);
+      displayNone(video);
+      displayNone(question1);
+      displayFlex(question2);
+      displayNone(thanks);
+      displayBlock(prevBtn);
+      displayBlock(questionWrapper);
+      displayBlock(nextBtn);
+      currentStep = "QUESTION2";
+      break;
+    case "END":
+      displayNone(playButton);
+      displayNone(video);
+      displayNone(question1);
+      displayNone(question2);
+      displayNone(nextBtn);
+      displayBlock(thanks);
+      displayBlock(prevBtn);
+      currentStep = "END";
+      break;
+  }
+}
+
+const handleNext = () => {
+  changeStep(step[step.indexOf(currentStep) + 1]);
+}
+
+const handlePrev = () => {
+  changeStep(step[step.indexOf(currentStep) - 1]);
+}
+
+nextBtn.addEventListener('click', () => {
+  handleNext();
+})
+
+prevBtn.addEventListener('click', () => {
+  handlePrev();
+})
 
 let isCompletedVideo = false;
 
 playButton.addEventListener("click", () => {
-  playButton.style.display = "none";
-  video.style.display = "block";
+  changeStep(step[1]);
   video.play();
   setTimeout(() => {
-    video.style.display = 'none';
-    question1.style.display = 'flex';
-    questionWrapper.style.display = 'block';
+    changeStep(step[2])
   }, video.duration * 1000 + 1000)
 });
 
@@ -34,11 +131,9 @@ correctAnswers.forEach(item => {
         congratsGif.style.display= "none";
         congratsGif.style.animation = "slide-top-in 1s ease-in";
         if (question1.style.display === 'flex') {
-          question1.style.display = 'none';
-          question2.style.display = 'flex';
+          changeStep(step[3]);
         } else if (question2.style.display === 'flex') {
-          question2.style.display = 'none';
-          thanks.style.display = 'block';
+          changeStep(step[4]);
         }
       }, 1000)
     }, 6000)
@@ -55,11 +150,9 @@ wrongAnswers.forEach(item => {
         wrongGif.style.display= "none";
         wrongGif.style.animation = "slide-top-in 1s ease-in";
         if (question1.style.display === 'flex') {
-          question1.style.display = 'none';
-          question2.style.display = 'flex';
+          changeStep(step[3]);
         } else if (question2.style.display === 'flex') {
-          question2.style.display = 'none';
-          thanks.style.display = 'block';
+          changeStep(step[4]);
         }
       }, 1000)
     }, 3000)
